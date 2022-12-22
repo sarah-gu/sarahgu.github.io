fff59204fc0e54135e9cb317f72ddef203c4f862
Sarah Gu <guwen888@gmail.com>
2022-12-21 19:02:54 -0500

    setup files

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+if (
+  typeof window !== "undefined" &&
+  process.env.NODE_ENV === "development"
+  // && /VIVID_ENABLED=true/.test(document.cookie)
+) {
+  import("vivid-studio").then((v) => v.run());
+  import("vivid-studio/style.css");
+}
+
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
